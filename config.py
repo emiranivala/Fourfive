d5a88c993793dba8ad82acad5317995162f91702
emiranivala <taitufam@gmail.com>
2025-01-19 10:25:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,4 +14,4 @@ FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "500"))
 PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "500000"))
 
 #AutoDeleteTime
-SECONDS = int(getenv("SECONDS", "527040")) #5_minutes
+SECONDS = int(getenv("SECONDS", "5270400000")) #5_minutes
