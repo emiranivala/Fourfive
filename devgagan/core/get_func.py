7d0a79fa7caaa98ab4ad28008c440e092ead325c
emiranivala <taitufam@gmail.com>
2025-01-29 17:41:15 +0530

    Update get_func.py

--- devgagan/core/get_func.py
+++ devgagan/core/get_func.py
@@ -212,6 +212,32 @@ async def copy_message_with_chat_id(client, sender, chat_id, message_id):
         
         replacements = load_replacement_words(sender)
         for word, replace_word in replacements.items():
+            final_caption = final_caption.replace(word, replace_word)
+        
+        caption = f"{final_caption}\n\n__**{custom_caption}**__" if custom_caption else f"{final_caption}"
+        
+        if msg.media:
+            if msg.media == MessageMediaType.VIDEO:
+                result = await client.send_video(target_chat_id, msg.video.file_id, caption=caption)
+            elif msg.media == MessageMediaType.DOCUMENT:
+                if msg.document.file_size > 1_073_741_824:  # 1GB in
+
+async def copy_message_with_chat_id(client, sender, chat_id, message_id):
+    target_chat_id = user_chat_ids.get(sender, sender)
+    
+    try:
+        msg = await client.get_messages(chat_id, message_id)
+        
+        custom_caption = get_user_caption_preference(sender)
+        original_caption = msg.caption if msg.caption else ''
+        final_caption = f"{original_caption}" if custom_caption else f"{original_caption}"
+        
+        delete_words = load_delete_words(sender)
+        for word in delete_words:
+            final_caption = final_caption.replace(word, '  ')
+        
+        replacements = load_replacement_words(sender)
+        for word, replace_word in replacements.items():
             final_caption = final_caption replace(word, replace_word)
         
         caption = f"{final_caption}\n\n__**{custom_caption}**__" if custom_caption else f"{final_caption}"
